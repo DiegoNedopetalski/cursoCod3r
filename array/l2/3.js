1bb2d62a89acbb5fb60a9f024b594cb489335d12
Diego Nedopetalski <diegoluis.nedo@gmail.com>
2022-05-22 11:01:21 -0300

    25

---
+++ array/l2/3.js
@@ -0,0 +1,5 @@
+const calcularSalario = function(horasTrabalhadas, valorHoras) {
+    return console.log(`Salário igual à ${horasTrabalhadas * valorHoras}!`)
+}
+
+calcularSalario(150, 40.5)
---
+++ array/l2/4.js
@@ -0,0 +1,4 @@
+const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+const nomeDoMes = mes => console.log(meses[mes - 1])
+
+nomeDoMes(4)
