dd58e03576746743bb4b36ab814a3e0179d8d097
Diego Nedopetalski <diegoluis.nedo@gmail.com>
2022-05-22 10:49:52 -0300

    main

---
+++ array/l2/1.js
@@ -0,0 +1,9 @@
+const cumprimentar = function(nome) {
+    return console.log(`Olá, ${nome}!`)
+}
+
+cumprimentar('José')
+
+const cumprimentar2 = nome2 => console.log(`Olá, ${nome2}!`)
+
+cumprimentar2('Jorge')
---
+++ array/l2/2.js
@@ -0,0 +1,4 @@
+const converterAnosParaDias = anos => console.log(anos * 365)
+
+converterAnosParaDias(25)
+
