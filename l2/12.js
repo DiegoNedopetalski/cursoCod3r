26394347992a5eb101880e49553c4096894b8970
Diego Nedopetalski <diegoluis.nedo@gmail.com>
2022-05-24 11:35:17 -0300

    main

--- l2/12.js
+++ l2/12.js
@@ -1,6 +1,20 @@
-function filtrarNumeros(item) {
+function filtrarNumeros(array) {
     const resultado = []
-    if (typeof item)
+    for (let item of array) {
+        if(typeof item === 'number') {
+            resultado.push(item)
+        }
+    }
+    return console.log(resultado)
 }
 
 filtrarNumeros(["Javascript", 1, "3", "Web", 20])
+
+// resolução 2
+/*
+function filtrarNumeros2(array) {
+    return array.filter(a => tipeof a === 'number')
+}
+
+filtrarNumeros2(["Javascript", 1, "3", "Web", 20])
+*/
