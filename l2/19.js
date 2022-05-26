68f3c4330803dd15d96f52caf1fe1c013953266c
Diego Nedopetalski <diegoluis.nedo@gmail.com>
2022-05-26 15:09:13 -0300

    main

--- l2/19.js
+++ l2/19.js
@@ -1,4 +1,7 @@
-
+function areaDoTriangulo(base, altura) {
+    let resultado = base * altura / 2
+    console.log(resultado.toFixed(2))
+}
 
 areaDoTriangulo(10, 15) // retornará 75.00
 areaDoTriangulo(7, 9) // retornará 31.50
