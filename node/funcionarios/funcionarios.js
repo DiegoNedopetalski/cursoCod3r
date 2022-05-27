90d2b5a5600cdd3d97eebaeedcf6a222304a2cf5
Diego Nedopetalski <diegoluis.nedo@gmail.com>
2022-05-27 09:53:37 -0300

    main

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -1,29 +1,21 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const chineses = f => f.pais ==='China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
 axios.get(url).then(response => {
     const funcionarios = response.data
 
-    function mulherChinesaMenosPaga(array) {
-        const mulherChinesa = []
-        for (item of array) {
-            if (item.genero === 'F' && item.pais === 'China') {
-                mulherChinesa.push(item)
-            }
-        }
-        const menosPaga = mulherChinesa[0]
-        for (item of mulherChinesa) {
-            if (menosPaga < item) {
-                menosPaga = item
-            }
-        }
-
-        return console.log(menosPaga)
-    }
-    
-    mulherChinesaMenosPaga(funcionarios);
+    //mulher chinesa com menor salário
+    const func = funcionarios
+    .filter(chineses)
+    .filter(mulheres)
+    .reduce(menorSalario)
+    console.log(func)
 })
 
-
-//mulher chinesa com menor salário
 
