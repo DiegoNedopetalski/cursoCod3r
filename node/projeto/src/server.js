dc7124374e07df10e8bf97a06aac3c5d28cf17d1
Diego Nedopetalski <diegoluis.nedo@gmail.com>
2022-05-28 11:28:56 -0300

    main

--- node/projeto/src/server.js
+++ node/projeto/src/server.js
@@ -2,10 +2,16 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser'); 
 const bancoDeDados = require('./bancoDeDados')
 
+app.use(express.json()); 
+app.use(express.urlencoded({
+  extended: true 
+})); 
 
-app.get('/projetos', (req, res, next) => {
+
+app.get('/produtos', (req, res, next) => {
     res.send(bancoDeDados.getProdutos())
 })
 
@@ -15,9 +21,23 @@ app.get('/produtos/:id', (req, res, next) => {
 
 app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     })
+    res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
     res.send(produto) // JSON
 })
 
@@ -25,3 +45,33 @@ app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}`)
 })
 
+/*
+
+
+// Express v4.16.0 e superior
+// -------------------------- 
+const express = require('express'); 
+app.use(express.json()); 
+app.use(express.urlencoded({
+  extended: true 
+})); 
+
+
+// For Express 4.16.0 ou abaixo 
+// ------------------------------------ 
+const bodyParser = require('body-parser'); 
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({
+  extended: true 
+}));
+
+
+
+
+// E na hora de usar, seria algo como:
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+*/
