3ee89b82f24ea22f10ebea8f5d42d55b382a0f8f
Diego Nedopetalski <diegoluis.nedo@gmail.com>
2022-10-31 16:11:07 -0300

    main

--- ajax/server.js
+++ ajax/server.js
@@ -37,4 +37,11 @@ app.post('/upload', (req, res) => {
     })
 })
 
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 7
+    })
+})
+
 app.listen(8080, () => console.log('Executando...'))
