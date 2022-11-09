6e3d2decca937055116df02fae72259c7b062344
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-11-09 15:21:06 -0300

    main

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,24 @@
+const modoDev = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+              }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -19,9 +31,12 @@ module.exports = {
             use: [
                 MiniCssExtractPlugin.loader,
                 //'style-loader', // Adiciona CSS a DOM injetando a tag <style>
-                'css-loader', // interpreta @impórt, url()...
+                'css-loader', // interpreta @import, url()...
                 'sass-loader'
             ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
         }]
     }
 }
