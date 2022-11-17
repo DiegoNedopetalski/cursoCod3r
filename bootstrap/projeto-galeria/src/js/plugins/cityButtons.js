1926e916e841b5b39f6c87aabc96d180ea909663
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-11-16 21:58:46 -0300

    main

--- bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
+++ bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
@@ -3,13 +3,17 @@ import $ from 'jquery'
 const duration = 600
 
 function filterByCity(city) {
-    $('[wm-city]').each(function(i, e) {
+    $('[wm-city]').each(function (i, e) {
         const isTarget = $(this).attr('wm-city') === city
             || city === null
-        if(isTarget) {
+        if (isTarget) {
+            $(this).parent().removeClass('d-none')
             $(this).fadeIn(duration)
         } else {
-            $(this).fadeOut(duration)
+            $(this).fadeOut(duration, () => {
+                $(this).parent()
+            })
         }
     })
 }
+
