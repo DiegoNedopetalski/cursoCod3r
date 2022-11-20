ae36a26975d8f9a3b44ab13bc62986557a89f8de
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-11-20 20:46:02 -0300

    Exercicios React - aula 2

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Primeiro from './components/Primeiro'
 
-const elemento = <h1>React 2</h1>
 
-ReactDOM.render(elemento, document.getElementById('root'))
+ReactDOM.render(<Primeiro />, document.getElementById('root'))
 
 // $(1<h1>).html('React 2')
