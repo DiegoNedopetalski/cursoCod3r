56be3ac9e91d2b128535d188a77daa24414c9dc0
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2022-11-22 12:03:11 -0300

    Estilizando os buttons

--- react/calculadora/src/components/Button.jsx
+++ react/calculadora/src/components/Button.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Button.css';
 
-export default function mainButtons(props){ 
-    return <button className='button'>{props.label}</button>
+export default function Buttons(props) {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return <button
+        onClick={e => props.click && props.click(props.label)}
+        className={classes}>
+        {props.label}
+    </button>
 }
