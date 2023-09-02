640dadb51f39c8a8b9564c89141ca8dea642d4c0
DiegoNedopetalski <diegoluis.nedo@gmail.com>
2023-09-02 04:01:37 -0300

    main

--- vue/montyhall/src/App.vue
+++ vue/montyhall/src/App.vue
@@ -1,12 +1,41 @@
 <template>
     <div id="app">
         <h1>Problema de Monty Hall</h1>
+        <div class="form">
+            <div v-if="!started">
+                <label for="doorsAmount">Quantas portas?</label>
+                <input type="text" id="doorsAmount" size="3"
+                    v-model.number="doorsAmount">
+            </div>
+            <div v-if="!started">
+                <label for="selectedDoor">Qual a porta premiada?</label>
+                <input type="text" id="selectedDoor" size="3"
+                    v-model.number="selectedDoor">
+            </div>
+            <button v-if="!started" @click="started = true">Iniciar</button>
+            <button v-if="started" @click="started =false">Reiniciar</button>
+        </div>
+        <div class="doors" v-if="started">
+            <div v-for="i in doorsAmount" :key="i">
+                <DoorMonty :hasGift="i === selectedDoor" :number="i" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import DoorMonty from './components/Door.vue'
+
 export default {
-    
+    name: 'App',
+    components: { DoorMonty },
+    data: function() {
+        return {
+            started: false,
+            doorsAmount: 3,
+            selected: null
+        }
+    }
 };
 </script>
 
@@ -32,5 +61,25 @@ body {
     background-color: #0004;
     padding: 20px;
     margin-bottom: 60px;
+}
+
+.form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+}
+
+.form, .form input, .form button {
+    margin-bottom: 10px;
+    font-size: 2rem;
+}
+
+.doors {
+    align-self: stretch;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
 }
 </style>
